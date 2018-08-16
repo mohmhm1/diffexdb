4c370d1b1b7684181c902c70ffeaf8f84da8047b
Ahmed Mahmoud <ahmedmahmoud86@ymail.com>
2018-08-16 17:01:35 -0400

    init3

---
+++ init.R
@@ -0,0 +1,13 @@
+my_packages = c("data.table","twitteR","RColorBrewer"."plotly","Ckmeans.1d.dp","survival",
+"dplyr","ggplot2",ggfortify)
+
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
